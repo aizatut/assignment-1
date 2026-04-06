1bfb6d1d8e713c4700935ed21b8b217082315530
utemi <252727@astanait.edu.kz>
2026-04-06 21:20:56 +0500

    fixed decimal

--- src/DecimalBinary.java
+++ src/DecimalBinary.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class DecimalBinary {
 
     static String dec2bin(int n) {
+        if (n == 0) {
+            return "0";
+        }
         if (n == 1) {
             return "1";
         }
-
-        return dec2bin(n / 2) + n % 2;
+        return dec2bin(n / 2) + (n % 2);
     }
 
     public static void main(String[] args) {
@@ -16,10 +18,9 @@ public class DecimalBinary {
         System.out.print("enter number: ");
         int n = sc.nextInt();
 
-        String res = dec2bin(n);
 
-        System.out.println("binary: " + res);
-
+        String result = dec2bin(n);
+        System.out.println("binary = " + result);
         sc.close();
     }
 }
