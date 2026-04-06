ba0329b376fa9926d3c303b7ce49fe7291f0d178
utemi <252727@astanait.edu.kz>
2026-04-06 23:33:18 +0500

    fix

--- src/SumSeries.java
+++ src/SumSeries.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class SumSeries {
     public static double sum1(int i) {
         if (i == 1) {
-            return 1.0;
+            return 1;
         }
         return sum1(i - 1) + 1.0 / i;
     }
     public static double sum2(int i) {
         if (i == 1) {
-            return 1.0 / 3;
+            return 1/ 3;
         }
         return sum2(i - 1) + (double) i / (2 * i + 1);
     }
     public static double sum3(int i) {
         if (i == 1) {
-            return 1.0 / 2;
+            return 1/ 2;
         }
         return sum3(i - 1) + (double) i / (i + 1);
     }
